94a611426f1de3f0263c572d3b0b81dd1f2ff986
storm772 <93355393+storm772@users.noreply.github.com>
2022-04-08 17:59:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,30 @@ def calc(n):
 
     if '10^' in n: isAlready = True
 
-    # Check if its already a scientific notation
+    # Scientific Notation Fixer
     if isAlready:
         found = 0
         decimal = False
         
         if target[0] == '0': decimal = True
         else: decimal = False
-        
+
+        if decimal == True:
+            zeroCount = 0
+            found = target.lstrip('0')
+            if int(found) > 10:
+                number = found[:1] + '.' + found[1:]
+                found = target[:1] + '.' + target[1:]
+                
+            for i in target:
+                if i == '0':
+                    zeroCount -=1
+
+            print(f'Zeros: {zeroCount}')
+            print(f'Target: {target}')
+            print(f'Number: {number}')
+            print(f'{number}x10^{exponent + zeroCount}')
+            
         if decimal == False:
             found = target.rstrip('0')
             if int(found) > 10:
@@ -23,7 +39,6 @@ def calc(n):
 
                 print(f'{number}x10^{len(found.split(".")[1]) + exponent}')
             else:
-                # if its a single number, like (1000 or 90000000)
                 zeroCount = 0
                 for i in target:
                     if i == '0':
@@ -31,4 +46,4 @@ def calc(n):
                 else:
                     print(f'{target.split("0")[0]}x10^{zeroCount}')
                 
-calc('1740000x10^3')
+calc('0,00054535432x10^-3')
