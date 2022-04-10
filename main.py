4313a382e87126493e769f9c3007bc663e61fb92
storm772 <93355393+storm772@users.noreply.github.com>
2022-04-09 22:15:08 -0300

    Refactored the code

--- main.py
+++ main.py
@@ -1,64 +1,49 @@
-# Main function
 def calc(n):
-
+    parts = n.replace('*','x').split('x10^')
+    number = parts[0]
     # Scientific Notation Fixer
-    if '10^' in n:
-        target = n.split('x')[0].replace(',','')
-        exponent = int(n.split('^')[1])
-        
-        # Decimal System
-        if target[0] == '0': 
+    if len(parts) == 2:
+        exponent = int(parts[1])
+        # Decimal
+        if number[0] == '0':
             zeroCount = 0
-            for i in target:
+            for i in number:
                 if i == '0': zeroCount -= 1
-                if i != '0': break
-
-            found = target.lstrip('0')
-
-            if int(found) > 10: 
-                found = found[:1] + '.' + found[1:]
-            print(f'{found}x10^{zeroCount + exponent}')
+                if i != '0': break 
             
-        else: 
-        # Integer System
-            found = target.rstrip('0')
-            if int(found) > 10:
-                number = found[:1] + '.' + found[1:]
-                found = target[:1] + '.' + target[1:]
-
-                print(f'{number}x10^{len(found.split(".")[1]) + exponent}')
-            elif int(found) < 10 and len(target) > 1:
-                zeroCount = 0
-                for i in target:
-                    if i == '0': zeroCount += 1
-                    if i != '0': break
-                
-                print(f'{target.split("0")[0]}x10^{zeroCount}')
+            found = number.lstrip('0')
+            if int(found) > 10:     found = f'{found[:1]}.{found[1:]}'
+            
+            print(f'{found}x10^{zeroCount + exponent}')
+        # Integer   
+        else:
+            found = number.rstrip('0')
+            if int(found) > 10:     
+                found = f'{number[:1]}.{number[1:]}'
+                print(f'{found.rstrip("0")}x10^{len(found.split(".")[1]) + exponent}')
             else:
-                raise ArithmeticError('Already converted')
+                print('hi')
 
-    # Scientific Notation Converter
-    if '10^' not in n: 
-        n = n.replace(' ','').replace(',','').replace('.','')
-        
-        # Decimal System
-        if n[0] == '0':
+    elif len(parts) == 1:
+        # Decimal
+        if number[0] == '0':
             zeroCount = 0
-            number = n.lstrip('0')
+            value = number.lstrip('0')
+
+            if int(value) > 10:
+                value = f'{value[:1]}.{value[1:]}'
+
+            for i in number:
+                if i == '0': zeroCount -= 1
+                if i != '0': break 
+            
+            print(f'{value}x10^{zeroCount}')
+        # Integer
+        else:
+           
             if int(number) > 10:
                 number = number[:1] + '.' + number[1:]
-            for i in n:
-                if i == '0': zeroCount -= 1
-                if i != '0': break
+                temp = number.rstrip('0')
+            print(f'{temp}x10^{len(number.split(".")[1])}')
 
-            print(f'{number}x10^{zeroCount}')
-            
-        # Integer System
-        elif n[0] != '0':
-            if int(n) > 10:
-                n = n[:1] + '.' + n[1:]
-            print(f'{n.rstrip("0")}x10^{len(n.split(".")[1])}')
-        else:
-            print('Invalid Operation')
-
-calc('00012')
+calc('720000')
